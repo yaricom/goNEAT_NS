c41999916816f92c7d32eba18cf4ce288f05502f
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-06-22 19:08:10 +0300

    Maze environment related methods moved into separate file. Common maze simulation methods implemented.

---
+++ experiments/maze/environment.go
@@ -0,0 +1,456 @@
+package maze
+
+import (
+	"math"
+	"errors"
+	"fmt"
+	"io"
+	"bufio"
+	"strings"
+)
+
+// The maximal allowed speed for maze agent
+const maxAgentSpeed = 3.0
+
+// The simple point class
+type Point struct {
+	X, Y float64
+}
+
+// Reads Point from specified reader
+func ReadPoint(lr io.Reader) Point {
+	point := Point{}
+	fmt.Fscanf(lr, "%f %f", &point.X, &point.Y)
+
+	return point
+}
+
+// To determine angle in degrees of vector defined by (0,0)->This Point. The angle is from 0 to 360 degrees anti clockwise.
+func (p Point) Angle() float64 {
+	ang := math.Atan2(p.Y, p.X) / math.Pi * 180.0
+	if ang < 0.0 {
+		// lower quadrants (3 and 4)
+		return ang + 360.0
+	}
+	return ang
+}
+
+// To rotate this point around another point with given angle in degrees
+func (p *Point) Rotate(angle float64, point Point) {
+	rad := angle / 180.0 * math.Pi
+	p.X -= point.X
+	p.Y -= point.Y
+
+	ox, oy := p.X, p.Y
+	p.X = math.Cos(rad) * ox - math.Sin(rad) * oy
+	p.Y = math.Sin(rad) * ox + math.Cos(rad) * oy
+
+	p.X += point.X
+	p.Y += point.Y
+}
+
+// To find distance between this point and another point
+func (p Point) Distance(point Point) float64 {
+	dx := point.X - p.X
+	dy := point.Y - p.Y
+	return math.Sqrt(dx * dx + dy * dy)
+}
+
+// The simple line segment class, used for maze walls
+type Line struct {
+	A, B Point
+}
+
+// To create new line
+func NewLine(a, b Point) Line {
+	return Line{A:a, B:b}
+}
+
+// Reads line from specified reader
+func ReadLine(lr io.Reader) Line {
+	a := Point{}
+	b := Point{}
+	fmt.Fscanf(lr, "%f %f %f %f", &a.X, &a.Y, &b.X, &b.Y)
+
+	return NewLine(a, b)
+}
+
+// To find midpoint of the line segment
+func (l Line) Midpoint() Point {
+	midpoint := Point{}
+	midpoint.X = (l.A.X + l.B.X) / 2.0
+	midpoint.Y = (l.A.Y + l.B.Y) / 2.0
+	return midpoint
+}
+
+// Returns point of intersection between two line segments if it exists
+func (l Line) Intersection(line Line) (bool, Point) {
+	pt := Point{}
+	A, B, C, D := l.A, l.B, line.A, line.B
+
+	rTop := (A.Y - C.Y) * (D.X - C.X) - (A.X - C.X) * (D.Y - C.Y)
+	rBot := (B.X - A.X) * (D.Y - C.Y) - (B.Y - A.Y) * (D.X - C.X)
+
+	sTop := (A.Y - C.Y) * (B.X - A.X) - (A.X - C.X) * (B.Y - A.Y)
+	sBot := (B.X - A.X) * (D.Y - C.Y) - (B.Y - A.Y) * (D.X - C.X)
+
+	if rBot == 0 || sBot == 0 {
+		// lines are parallel
+		return false, pt
+	}
+
+	r := rTop / rBot
+	s := sTop / sBot
+	if r > 0 && r < 1 && s > 0 && s < 1 {
+		pt.X = A.X + r * (B.X - A.X)
+		pt.Y = A.Y + r * (B.Y - A.Y)
+
+		return true, pt
+	}
+	return false, pt
+}
+
+// To find distance between line segment and the point
+func (l Line) Distance(p Point) float64 {
+	utop := (p.X - l.A.X) * (l.B.X - l.A.X) + (p.Y - l.A.Y) * (l.B.Y - l.A.Y)
+	ubot := l.A.Distance(l.B)
+	ubot *= ubot
+	if ubot == 0.0 {
+		return 0.0
+	}
+
+	u := utop / ubot
+	if u < 0 || u > 1 {
+		d1 := l.A.Distance(p)
+		d2 := l.B.Distance(p)
+		if d1 < d2 {
+			return d1
+		}
+		return d2
+	}
+	point := Point{}
+	point.X = l.A.X + u * (l.B.X - l.A.X)
+	point.Y = l.A.Y + u * (l.B.Y - l.A.Y)
+	return point.Distance(p)
+}
+
+// The line segment length
+func (l Line) Length() float64 {
+	return l.A.Distance(l.B)
+}
+
+// The class for the maze navigator agent
+type Agent struct {
+	// The current location
+	Location          Point
+	// The heading direction in degrees
+	Heading           float64
+	// The speed of agent
+	Speed             float64
+	// The angular velocity
+	AngularVelocity   float64
+	// The radius of agent body
+	Radius            float64
+	// The maximal range of range finder sensors
+	RangeFinderRange  float64
+
+	// The angles of range finder sensors
+	RangeFinderAngles []float64
+	// The beginning angles for radar sensors
+	RadarAngles1      []float64
+	// The ending angles for radar sensors
+	RadarAngles2      []float64
+
+	// stores radar outputs
+	Radar             []float64
+	// stores rangefinder outputs
+	RangeFinders      []float64
+}
+
+// Creates new Agent with default settings
+func NewAgent() Agent {
+	agent := Agent{
+		Heading:0.0,
+		Speed:0.0,
+		AngularVelocity:0.0,
+		Radius:8.0,
+		RangeFinderRange:100.0,
+	}
+
+	// define the range finder sensors
+	agent.RangeFinderAngles = []float64{-90.0, -45.0, 0.0, 45.0, 90.0, -180.0}
+
+	// define the radar sensors
+	agent.RadarAngles1 = []float64{315.0, 45.0, 135.0, 225.0}
+	agent.RadarAngles2 = []float64{405.0, 135.0, 225.0, 315.0}
+
+	agent.RangeFinders = make([]float64, len(agent.RangeFinderAngles))
+	agent.Radar = make([]float64, len(agent.RadarAngles1))
+
+	return agent
+}
+
+// The maze environment class
+type Environment struct {
+	// The maze navigating agent
+	Hero      Agent
+	// The maze line segments
+	Lines     []Line
+	// The maze exit - goal
+	MazeExit  Point
+
+	// The flag to indicate if exit was found
+	ExitFound bool
+}
+
+// Reads maze environment from reader
+func ReadEnvironment(ir io.Reader) (*Environment, error) {
+	env := Environment{}
+	env.Hero = NewAgent()
+	env.Lines = make([]Line, 0)
+
+	// Loop until file is finished, parsing each line
+	scanner := bufio.NewScanner(ir)
+	scanner.Split(bufio.ScanLines)
+	index, numLines := 0, 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		lr := strings.NewReader(line)
+		switch index {
+		case 0:// read in how many line segments
+			fmt.Fscanf(lr, "%d", &numLines)
+
+		case 1:// read initial agent's location
+			env.Hero.Location = ReadPoint(lr)
+
+		case 2:// read initial heading
+			fmt.Fscanf(lr, "%f", &env.Hero.Heading)
+
+		case 3:// read the maze exit location
+			env.MazeExit = ReadPoint(lr)
+
+		default:
+			// read maze line segments
+			if numLines > 0 {
+				env.Lines = append(env.Lines, ReadLine(lr))
+				numLines--
+			}
+		}
+
+		index++
+	}
+
+	// update sensors
+	err := env.updateRangefinders()
+	if err != nil {
+		return nil, err
+	}
+	env.updateRadar()
+
+	return &env, nil
+}
+
+// create neural net inputs from maze agent sensors
+func (e *Environment) GetInputs() ([]float64, error) {
+	inputs_size := len(e.Hero.RangeFinders) + len(e.Hero.Radar) + 1
+	inputs := make([]float64, inputs_size)
+	// bias
+	inputs[0] = 1.0
+
+	// range finders
+	i := 0
+	for ; i < len(e.Hero.RangeFinders); i++ {
+		inputs[1 + i] = e.Hero.RangeFinders[i] / e.Hero.RangeFinderRange
+		if math.IsNaN(inputs[1 + i]) {
+			return nil, errors.New("NAN in inputs from range finders")
+		}
+	}
+
+	// radar
+	for j := 0; j < len(e.Hero.Radar); j++ {
+		inputs[i + j] = e.Hero.Radar[j]
+		if math.IsNaN(inputs[i + j]) {
+			return nil, errors.New("NAN in inputs from radar")
+		}
+	}
+
+	return inputs, nil
+}
+
+// transform neural net outputs into angular velocity and speed
+func (e *Environment) ApplyOutputs(o1, o2 float64) error {
+	if math.IsNaN(o1) || math.IsNaN(o2) {
+		return errors.New("OUTPUT is NAN")
+	}
+
+	e.Hero.AngularVelocity += (o1 - 0.5)
+	e.Hero.Speed += (o2 - 0.5)
+
+	// constraints of speed & angular velocity
+	if e.Hero.Speed > maxAgentSpeed {
+		e.Hero.Speed = maxAgentSpeed
+	}
+	if e.Hero.Speed < -maxAgentSpeed {
+		e.Hero.Speed = -maxAgentSpeed
+	}
+	if e.Hero.AngularVelocity > maxAgentSpeed {
+		e.Hero.AngularVelocity = maxAgentSpeed
+	}
+	if e.Hero.AngularVelocity < -maxAgentSpeed {
+		e.Hero.AngularVelocity = -maxAgentSpeed
+	}
+
+	return nil
+}
+
+// Do one time step of the simulation
+func (e *Environment) Update() error {
+	if e.ExitFound {
+		return nil
+	}
+
+	// get horizontal and vertical velocity components
+	vx := math.Cos(e.Hero.Heading / 180.0 * math.Pi) * e.Hero.Speed
+	vy := math.Sin(e.Hero.Heading / 180.0 * math.Pi) * e.Hero.Speed
+
+	if math.IsNaN(vx) {
+		return errors.New("VX NAN")
+	}
+	if math.IsNaN(vy) {
+		return errors.New("VY NAN")
+	}
+
+	// Update agent heading
+	e.Hero.Heading += e.Hero.AngularVelocity
+	if math.IsNaN(e.Hero.AngularVelocity) {
+		return errors.New("HERO ANG VEL NAN")
+	}
+
+	if e.Hero.Heading > 360 {
+		e.Hero.Heading -= 360
+	}
+	if e.Hero.Heading < 0 {
+		e.Hero.Heading += 360
+	}
+
+	// Find next agent's location
+	newloc := Point{
+		X:vx + e.Hero.Location.X,
+		Y:vy + e.Hero.Location.Y,
+	}
+	if !e.testAgentCollision(newloc) {
+		e.Hero.Location.X = newloc.X
+		e.Hero.Location.Y = newloc.Y
+	}
+	err := e.updateRangefinders()
+	if err != nil {
+		return err
+	}
+	e.updateRadar()
+
+	return nil
+}
+
+// used for fitness calculations based on distance of maze Agent to the target maze exit
+func (e *Environment) distanceToExit() (float64, error) {
+	dist := e.Hero.Location.Distance(e.MazeExit)
+	if math.IsNaN(dist) {
+		return 500, errors.New("NAN Distance error...")
+	}
+	if dist < 5.0 {
+		e.ExitFound = true //if within 5 units, success!
+	}
+	return dist, nil
+}
+
+// update rangefinder sensors
+func (e *Environment) updateRangefinders() error {
+	// iterate through each sensor and find distance to maze lines with agent's range finder sensors
+	for i := 0; i < len(e.Hero.RangeFinderAngles); i++ {
+		// radians...
+		rad := e.Hero.RangeFinderAngles[i] / 180.0 * math.Pi
+
+		// project a point from the hero's location outwards
+		projection_point := Point{
+			X:e.Hero.Location.X + math.Cos(rad) * e.Hero.RangeFinderRange,
+			Y:e.Hero.Location.Y + math.Sin(rad) * e.Hero.RangeFinderRange,
+		}
+		// rotate the projection point by the hero's heading
+		projection_point.Rotate(e.Hero.Heading, e.Hero.Location)
+
+		// create a line segment from the hero's location to projected
+		projection_line := Line{
+			A:e.Hero.Location,
+			B:projection_point,
+		}
+
+		// set range to max by default
+		min_range := e.Hero.RangeFinderRange
+
+		// now test against the environment to see if we hit anything
+		for j := 0; j < len(e.Lines); j++ {
+			found, intersection := e.Lines[j].Intersection(projection_line)
+			if found {
+				// if so, then update the range to the distance
+				found_range := intersection.Distance(e.Hero.Location)
+
+				// we want the closest intersection
+				if found_range < min_range {
+					min_range = found_range
+				}
+			}
+		}
+
+		if math.IsNaN(min_range) {
+			return errors.New("RANGE is NAN")
+		}
+		e.Hero.RangeFinders[i] = min_range
+	}
+	return nil
+}
+
+// update radar sensors
+func (e *Environment) updateRadar() {
+	target := e.MazeExit
+
+	// rotate goal with respect to heading of agent
+	target.Rotate(-e.Hero.Heading, e.Hero.Location)
+
+	// translate with respect to location of agent
+	target.X -= e.Hero.Location.X
+	target.Y -= e.Hero.Location.Y
+
+	// what angle is the vector between target & agent
+	angle := target.Angle()
+
+	// fire the appropriate radar sensor
+	for i := 0; i < len(e.Hero.RadarAngles1); i++ {
+		e.Hero.Radar[i] = 0.0
+
+		if (angle >= e.Hero.RadarAngles1[i] && angle < e.Hero.RadarAngles2[i]) ||
+			(angle + 360.0 >= e.Hero.RadarAngles1[i] && angle + 360.0 < e.Hero.RadarAngles2[i]) {
+			e.Hero.Radar[i] = 1.0
+		}
+	}
+}
+
+// see if provided new location hits anything in maze
+func (e *Environment) testAgentCollision(loc Point) bool {
+	for j := 0; j < len(e.Lines); j++ {
+		if e.Lines[j].Distance(loc) < e.Hero.Radius {
+			return true
+		}
+	}
+	return false
+}
+
+// Stringer
+func (e *Environment) String() string {
+	str := fmt.Sprintf("MAZE\nHero at: %.1f, %.1f\n", e.Hero.Location.X, e.Hero.Location.Y)
+	str += fmt.Sprintf("Exit at: %.1f, %.1f\n", e.MazeExit.X, e.MazeExit.Y)
+	str += "Lines:\n"
+	for _, l := range e.Lines {
+		str += fmt.Sprintf("\t[%.1f, %.1f] -> [%.1f, %.1f]\n", l.A.X, l.A.Y, l.B.X, l.B.Y)
+	}
+	return str
+}
---
+++ experiments/maze/environment_test.go
@@ -0,0 +1,257 @@
+package maze
+
+import (
+	"testing"
+	"os"
+	"strings"
+)
+
+func TestPoint_Angle(t *testing.T) {
+	var logError = func(angle, expected float64) {
+		t.Errorf("Wrong angle found: %f, expected: %f\n", angle, expected)
+	}
+	// 0 degrees
+	p := Point{1.0, 0.0}
+	angle := p.Angle()
+	if angle != 0 {
+		logError(angle, 0.0)
+	}
+	// 90 degrees
+	p.X = 0
+	p.Y = 1.0
+	angle = p.Angle()
+	if angle != 90 {
+		logError(angle, 90.0)
+	}
+	// 180 degrees
+	p.X = -1.0
+	p.Y = 0.0
+	angle = p.Angle()
+	if angle != 180 {
+		logError(angle, 180.0)
+	}
+	// 270 degrees
+	p.X = 0
+	p.Y = -1.0
+	angle = p.Angle()
+	if angle != 270 {
+		logError(angle, 270.0)
+	}
+
+	// 45 degrees
+	p.X = 1.0
+	p.Y = 1.0
+	angle = p.Angle()
+	if angle != 45 {
+		logError(angle, 45.0)
+	}
+	// 135 degrees
+	p.X = -1.0
+	p.Y = 1.0
+	angle = p.Angle()
+	if angle != 135 {
+		logError(angle, 135.0)
+	}
+	// 225 degrees
+	p.X = -1.0
+	p.Y = -1.0
+	angle = p.Angle()
+	if angle != 225 {
+		logError(angle, 225.0)
+	}
+	// 315 degrees
+	p.X = 1.0
+	p.Y = -1.0
+	angle = p.Angle()
+	if angle != 315 {
+		logError(angle, 315.0)
+	}
+}
+
+func TestPoint_Rotate(t *testing.T) {
+	p := Point{2.0, 1.0}
+
+	p.Rotate(90.0, Point{1.0, 1.0})
+
+	if p.X != 1.0 || p.Y != 2.0 {
+		t.Error("Wrong position after rotation", p)
+	}
+
+	p.Rotate(180.0, Point{1.0, 1.0})
+
+	if  1.0 - p.X > 0.00000001 || p.Y != 0.0 {
+		t.Error("Wrong position after rotation", p)
+	}
+}
+
+func TestPoint_Distance(t *testing.T) {
+	p := Point{2.0, 1.0}
+	p1 := Point{5.0, 1.0}
+
+	d := p.Distance(p1)
+	if d != 3 {
+		t.Error("Wrong distance", d)
+	}
+
+	p2 := Point{5.0, 3.0}
+	d = p.Distance(p2)
+	if d != math.Sqrt(13.0){
+		t.Error("Wrong distance", d)
+	}
+}
+
+func TestReadPoint(t *testing.T) {
+	str := "10 20"
+	lr := strings.NewReader(str)
+
+	point := ReadPoint(lr)
+	if point.X != 10 {
+		t.Errorf("Point has wrong X: %f, expected: %f\n", point.X, 10)
+	}
+	if point.Y != 20 {
+		t.Errorf("Point has wrong Y: %f, expected: %f\n", point.Y, 20)
+	}
+}
+
+func TestLine_Intersection(t *testing.T) {
+	l1 := Line{
+		A:Point{1.0, 1.0},
+		B:Point{5.0, 5.0},
+	}
+	l2 := Line{
+		A:Point{1.0, 5.0},
+		B:Point{5.0, 1.0},
+	}
+
+	// test intersection
+	found, p := l1.Intersection(l2)
+	if !found {
+		t.Error("Lines intesect")
+	}
+	if p.X != 3.0 || p.Y != 3.0 {
+		t.Error("Wrong intersection point found", p)
+	}
+
+	// test parallel
+	l3 := Line{
+		A:Point{2.0, 1.0},
+		B:Point{6.0, 1.0},
+	}
+	found, p = l1.Intersection(l3)
+	if found {
+		t.Error("Parallel lines do not intesect")
+	}
+	if p.X != 0 || p.Y != 0 {
+		t.Error("Wrong intersection point found", p)
+	}
+
+	// test no intersection by coordinates
+	l4 := Line{
+		A:Point{4.0, 4.0},
+		B:Point{6.0, 1.0},
+	}
+	found, p = l1.Intersection(l4)
+	if found {
+		t.Error("The lines must not intesect")
+	}
+	if p.X != 0 || p.Y != 0 {
+		t.Error("Wrong intersection point found", p)
+	}
+}
+
+func TestLine_Distance(t *testing.T) {
+	l := Line{
+		A:Point{1.0, 1.0},
+		B:Point{5.0, 1.0},
+	}
+
+	p := Point{4.0, 3.0}
+
+	d := l.Distance(p)
+	if d != 2.0 {
+		t.Errorf("Wrong distance from line to point: %f, expected: %f\n", d, 2.0)
+	}
+}
+
+func TestLine_Length(t *testing.T) {
+	l := Line{
+		A:Point{1.0, 1.0},
+		B:Point{5.0, 1.0},
+	}
+	length := l.Length()
+	if length != 4.0 {
+		t.Errorf("Wrong line length: %f, expected: %f\n", length, 4.0)
+	}
+}
+
+func TestReadLine(t *testing.T) {
+	str := "10 20 30 40"
+	lr := strings.NewReader(str)
+
+	line := ReadLine(lr)
+	if line.A.X != 10 {
+		t.Error("line.A.X != 10")
+	}
+	if line.A.Y != 20 {
+		t.Error("line.A.Y != 20")
+	}
+	if line.B.X != 30 {
+		t.Error("line.B.X != 30")
+	}
+	if line.B.Y != 40 {
+		t.Error("line.B.Y != 40")
+	}
+}
+
+func TestReadEnvironment(t *testing.T) {
+	maze_config_path := "../../data/medium_maze.txt"
+
+	// open maze config file
+	maze_file, err := os.Open(maze_config_path)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	env, err := ReadEnvironment(maze_file)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if env.Hero.Location.X != 30 {
+		t.Error("env.Hero.Location.X != 30")
+	}
+	if env.Hero.Location.Y != 22 {
+		t.Error("env.Hero.Location.Y != 22")
+	}
+	if len(env.Lines) != 11 {
+		t.Error("len(env.Lines) != 11")
+	}
+	if env.MazeExit.X != 270 {
+		t.Error("env.End.X != 270")
+	}
+	if env.MazeExit.Y != 100 {
+		t.Error("env.End.Y != 100")
+	}
+
+	lines := []Line{
+		{Point{293, 7}, Point{289, 130}},
+		{Point{289, 130}, Point{6, 134}},
+		{Point{6, 134}, Point{8, 5}},
+		{Point{8, 5}, Point{292, 7}},
+		{Point{241, 130}, Point{58, 65}},
+		{Point{114, 7}, Point{73, 42}},
+		{Point{130, 91}, Point{107, 46}},
+		{Point{196, 8}, Point{139, 51}},
+		{Point{219, 122}, Point{182, 63}},
+		{Point{267, 9}, Point{214, 63}},
+		{Point{271, 129}, Point{237, 88}},
+	}
+
+	for i, l := range env.Lines {
+		if lines[i].A.X != l.A.X || lines[i].A.Y != l.A.Y || lines[i].B.X != l.B.X || lines[i].B.Y != l.B.Y {
+			t.Error("Wrong line found", l)
+		}
+	}
+}
